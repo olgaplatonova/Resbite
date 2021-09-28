c8f7eca27ab94a1c50fac75d5fa749d8ac7daaaa
Ольга <olga@MacBook-Pro-Olga.local>
2021-09-28 13:29:41 +0300

    init-res

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// Плавная прокрутка при клике
+
+const menuLinks = document.querySelectorAll('.menu-item[data-goto]')
+
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
+
+// Меню бургер 
+
+const iconMenu = document.querySelector('.menu-icon');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu-body');
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock')
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+
+}
